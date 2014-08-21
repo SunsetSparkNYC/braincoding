5258e71a8cd6a841fcba60b42c0041d9f367bb9b
Gaelen Hadlett <gaelenh@gmail.com>
2014-08-21 18:53:49 -0400

    Adding netstation events

--- experiments/summer2014.py
+++ experiments/summer2014.py
@@ -2,6 +2,25 @@
 
 import random
 from psychopy import visual, sound, event, core
+import egi.threaded as egi
+
+try:
+    netstation = egi.Netstation()
+    netstation.initialize('11.0.0.42', 11535)
+    netstation.BeginSession()
+    netstation.sync()
+    netstation.StartRecording()
+except:
+    netstation = None
+
+
+def send_event(key, label):
+    if netstation is not None:
+        netstation.send_event(
+            key,
+            label=label,
+            timestamp=egi.ms_localtime(),
+        )
 
 my_window = visual.Window((500, 500), rgb=(0, 0, 0))
 sound_stim = sound.Sound(
@@ -18,8 +37,12 @@ for x in range(700):
     sound_stim.play()
     my_window.flip()
     core.wait(0.1)
+    send_event('SND' + new_tone, 'Playing %s' % new_tone)
     core.wait(sound_stim.getDuration() + 0.3)
 
     if 'q' in event.getKeys():
         my_window.close()
         core.quit()
+
+if netstation is not None:
+    netstation.StopRecording()
